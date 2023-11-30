dae1bc89e0a8812c97d13c96d1a38e23bc8b0342
JulioQuintana <juliocesarquintanasilva@gmail.com>
2023-11-30 17:16:31 -0300

    init commit

---
+++ ddd-architecture_hexagonal-practice/src/main/java/com/quintanasystem/dddarchitecture_hexagonalpractice/DddArchitectureHexagonalPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.quintanasystem.dddarchitecture_hexagonalpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddArchitectureHexagonalPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddArchitectureHexagonalPracticeApplication.class, args);
+	}
+
+}
---
+++ ddd-architecture_hexagonal-practice/src/test/java/com/quintanasystem/dddarchitecture_hexagonalpractice/DddArchitectureHexagonalPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quintanasystem.dddarchitecture_hexagonalpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddArchitectureHexagonalPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
