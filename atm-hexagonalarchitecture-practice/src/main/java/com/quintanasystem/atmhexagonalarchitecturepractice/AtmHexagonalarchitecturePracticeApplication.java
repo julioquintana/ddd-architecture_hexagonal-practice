4bd33d31f30534373e417a5a9200ad2733ac4ca3
JulioQuintana <juliocesarquintanasilva@gmail.com>
2023-12-01 16:47:02 -0300

    initial commit

---
+++ atm-hexagonalarchitecture-practice/src/main/java/com/quintanasystem/atmhexagonalarchitecturepractice/AtmHexagonalarchitecturePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.quintanasystem.atmhexagonalarchitecturepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmHexagonalarchitecturePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmHexagonalarchitecturePracticeApplication.class, args);
+    }
+
+}
---
+++ atm-hexagonalarchitecture-practice/src/test/java/com/quintanasystem/atmhexagonalarchitecturepractice/AtmHexagonalarchitecturePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quintanasystem.atmhexagonalarchitecturepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmHexagonalarchitecturePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
